ac7567a865ed4812c79b183a053b9bd3ecd1b88c
Hà <huahuuha2k4@gmail.com>
2024-12-06 12:14:14 +0700

    Initial commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.querySelectorAll("button").forEach((button)) => {
-    button.adEventListener("click"), () => {
+document.querySelectorAll("button").forEach((button) => {
+    button.addEventListener("click", () => {
         alert("Món ăn đã được thêm vào giỏ hàng!");
     });
 });
